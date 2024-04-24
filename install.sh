a72002e11403a4c86fa52758f5e6afffdeaebde3
rayylee <rayylee@foxmail.com>
2024-04-24 19:33:14 +0800

    update install.sh for install fzf
    Signed-off-by: rayylee <rayylee@foxmail.com>

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 cd $(dirname $0)
 
 if command -v hx >/dev/null 2>&1; then
-    echo -e "\033[33m- [Warn] hx command alreally exist\033[0m"
+    echo -e "\033[33m- [Warn] hx command already exist\033[0m"
 else
     check_os=$(rpm -qa 2>/dev/null | grep release | grep el9.x86_64)
     if [ -n "$check_os" ]; then
@@ -26,10 +26,9 @@ if ! command -v hx >/dev/null 2>&1; then
 fi
 
 if command -v rust-analyzer  >/dev/null 2>&1; then
-    echo -e "\033[33m- [Warn] rust-analyzer command alreally exist\033[0m"
+    echo -e "\033[33m- [Warn] rust-analyzer command already exist\033[0m"
 else
     if [ ! -e rust-analyzer-x86_64-unknown-linux-gnu.gz ]; then
-        gunzip rust-analyzer-x86_64-unknown-linux-gnu.gz
         wget https://github.com/rust-lang/rust-analyzer/releases/download/2024-01-29/rust-analyzer-x86_64-unknown-linux-gnu.gz
     fi
 
@@ -46,12 +45,45 @@ if ! command -v rust-analyzer >/dev/null 2>&1; then
     exit 1
 fi
 
+
+if command -v fzf >/dev/null 2>&1; then
+    echo -e "\033[33m- [Warn] fzf command already exist\033[0m"
+else
+    if [ ! -e fzf-0.50.0-linux_amd64.tar.gz ]; then
+        wget https://github.com/junegunn/fzf/releases/download/0.50.0/fzf-0.50.0-linux_amd64.tar.gz
+    fi
+
+    if [ -e fzf-0.50.0-linux_amd64.tar.gz ]; then
+        tar -xvf fzf-0.50.0-linux_amd64.tar.gz
+        chmod +x fzf
+        [ ! -d ~/.cargo/bin ] && mkdir -p ~/.cargo/bin
+        mv fzf ~/.cargo/bin/
+    fi
+fi
+if ! command -v fzf >/dev/null 2>&1; then
+    echo -e "\033[33m- [Err] fzf command install failed\033[0m"
+    exit 1
+fi
+
 if [ ! -d ~/.config/helix ]; then
     mkdir -p ~/.config/helix
 fi
 cp -f helix-vim/config.toml ~/.config/helix/
 
 cp -f ./languages.toml ~/.config/helix/
+
+[ ! -d /usr/local/bin ] && mkdir -p /usr/local/bin
+
+cat > /usr/local/bin/helix << EOF
+#!/bin/bash
+if [ -z "\$1" ]; then
+    hx \$(fzf 2>/dev/null)
+else
+    hx \$@
+fi
+exit 0
+EOF
+chmod +x /usr/local/bin/helix
 
 echo -e "\033[32m- [Info] Install successfully...\033[0m"
 
